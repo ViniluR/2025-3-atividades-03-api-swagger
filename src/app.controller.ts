1b9a55427ba67698b98d70dccb1b72db2b5c532f
ViniH <124372681+ViniluR@users.noreply.github.com>
2025-12-06 02:41:31 +0000

    documentado o endpoint raiz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('root')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Status.', description: 'Retorna status e metadata da API.' })
+  @ApiResponse({ status: 200, description: 'Status retornado com sucesso.' })
   getInfo() {
     return this.appService.getInfo();
   }
