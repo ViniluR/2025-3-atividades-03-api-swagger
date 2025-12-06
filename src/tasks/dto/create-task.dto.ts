95214db34935611011a92f7b5ecda89aaed2f3be
ViniH <124372681+ViniluR@users.noreply.github.com>
2025-12-06 02:38:36 +0000

    documentado os DTOs

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,15 +1,32 @@
 import { IsString, IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { TaskStatus } from '../task.entity';
 
 export class CreateTaskDto {
+  @ApiProperty({
+    description: 'Titulo',
+    example: 'Comprar leite',
+    minLength: 3,
+    maxLength: 100,
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'Descricao.',
+    example: 'decrever',
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiPropertyOptional({
+    description: 'Status inicial.',
+    enum: TaskStatus,
+    default: TaskStatus.ABERTO,
+    example: TaskStatus.ABERTO,
+  })
   @IsEnum(TaskStatus)
   @IsOptional()
   status?: TaskStatus;
